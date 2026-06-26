e41505be9f4beeccb00fdb350302de43d26b98cb
awsimkins <anthonysimkins@gmail.com>
2026-06-26 16:36:55 -0500

    Update credits: Ruby Rose prayers/confessions, Anthony worship, Suno production

--- partials/streaming-config.js
+++ partials/streaming-config.js
@@ -2,7 +2,7 @@ window.SD_STREAMING = {
     albumTitle: 'Prayers to the Father',
     albumSubtitle: 'Ten spoken prayers to the Father',
     artist: 'Spoken Devotion',
-    featuredArtist: 'Anna',
+    featuredArtist: 'Ruby Rose',
     presaveUrl: 'https://distrokid.com/hyperfollow/spokendevotion/prayers-to-the-father',
     releaseDate: '2026-07-03',
     releaseDateLabel: 'July 3, 2026'
